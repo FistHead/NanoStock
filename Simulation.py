fda6b20b72c38226b29a82739c8addde38cf1d9c
FistHead <nokick2806@gmail.com>
2026-03-01 21:54:00 +0300

    Initial commit

---
+++ Simulation.py
@@ -0,0 +1,244 @@
+import matplotlib.pyplot as plt
+import random
+from Stock import Coin,User
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+#--------------------------------------------------------
+class TradeNetLSTM(nn.Module):
+    def __init__(self, input_size,num_layers, hidden_size, output_size):
+        super(TradeNetLSTM, self).__init__()
+
+
+        self.lstm = nn.LSTM(input_size=input_size,
+                                    hidden_size=hidden_size,
+                                    num_layers=num_layers,
+                                    batch_first=True,
+                                    dropout=0.2)
+        self.fc = nn.Linear(hidden_size,hidden_size)
+
+        self.fc1 = nn.Linear(hidden_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = x.unsqueeze(-1)
+
+        _, (h_n, _) = self.lstm(x)
+
+        out = h_n[-1]
+
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+
+        first_part = out[:, :1]
+        other_part = out[:, 1:]
+
+        first_activated = torch.relu(first_part)
+        other_activated = torch.sigmoid(other_part)
+
+        return torch.cat((first_activated, other_activated), dim=1)
+
+#--------------------------------------------------------
+
+class TradeNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(TradeNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+
+
+        x = self.fc3(x)
+
+
+        first_part = x[:, :1]
+        other_part = x[:, 1:]
+
+
+        first_activated = torch.relu(first_part)
+        other_activated = torch.sigmoid(other_part)
+        return torch.cat((first_activated, other_activated), dim=1)
+
+
+#--------------------------------------------------------
+#функция обвала рынка
+def crash(coin_list,percentage):
+    for coin in coin_list:
+        before = coin.currency
+        coin.currency *= (1 - percentage/100)
+        # print(f'{coin.name} before:{before} after:{coin.currency}')
+
+#демонстрация
+# d_coin_list = [Coin(f'd_coin{i}',random.randint(10,100),random.randint(10,100)) for i in range(3)]
+# crash(d_coin_list,40)
+
+#--------------------------------------------------------
+#функция бума на рынке
+def stock_boom(coin_list,percentage):
+    for coin in coin_list:
+        before = coin.currency
+        coin.currency *= (1 + percentage/100)
+        # print(f'{coin.name} before:{before} after:{coin.currency}')
+
+#демонстрация
+# d_coin_list = [Coin(f'd_coin{i}',random.randint(10,100),random.randint(10,100)) for i in range(3)]
+# stock_boom(d_coin_list,10)
+
+#--------------------------------------------------------
+
+def main():
+    steps = 2000 # шаги
+    users_count = 4 # кол-во юзеров
+    coins_count = 4# кол-во коинов
+    ai_users_count = 4
+    percent_to_stock = 1.5 # процент в биржу
+
+    # создание главного коина
+    world_coin = Coin('world_coin',10**9,10**9)
+    world_manager = [0,[world_coin]]
+
+    users_list = []
+    ai_users = []
+
+
+    # создание событий
+    events = ['crash','boom','nothing']
+    events_weight = [5,5,90]
+
+    for i in range(users_count):
+        u = User('user'+str(i),1000,world_coin,world_manager[1])
+        u.initialize_nick()
+        u.register_in_wc()
+        users_list.append(u)
+
+    for i in range(ai_users_count):
+        n_path = f'megakoinT2.pth'
+        model = TradeNetLSTM(1,2,128,4)
+        model.load_state_dict(torch.load(n_path))
+        model.eval()
+        nick = f"{n_path.split('.')[0]}_{i}"
+
+        neuro_user = User(nick,1000,world_coin,world_manager[1])
+        neuro_user.initialize_nick()
+        neuro_user.register_in_wc()
+        users_list.append(neuro_user)
+        ai_users.append(neuro_user)
+
+
+    for i in range(coins_count):
+        world_manager[1].append(Coin(f'Coin{i}',random.randint(1000,100000),random.randint(1000,100000)))
+
+
+    history = {
+        'prices': {coin.name: [] for coin in world_manager[1]},
+        'wealth': {user.nickname: [] for user in users_list}
+    }
+
+    for step in range(steps):
+        event = random.choices(events,weights=events_weight,k=1)[0]
+
+        if event == 'crash':
+            crash(world_manager[1],random.randint(10,25))
+        elif event == 'boom':
+            stock_boom(world_manager[1],random.randint(5,30))
+
+        for user in users_list:
+
+            if user in ai_users:
+                print(user.nickname)
+                target_coin = random.choice(world_manager[1][1:])  # кроме world_coin
+                coin_history = history['prices'][target_coin.name]
+
+
+                if len(coin_history) >= 128:
+
+                    input_data = torch.tensor([coin_history[-128:]], dtype=torch.float32)
+
+
+                    input_data = input_data / (input_data[0, 0] + 1e-8)
+
+                    with torch.no_grad():
+                        # Предсказание: [Price_Ref, Buy_Prob, Sell_Prob, Hold_Prob]
+                        prediction = model(input_data)
+
+
+                    probs = prediction[0, 1:]
+                    action_idx = torch.argmax(probs).item()  # BuyCount, Buy, Sell, Hold
+
+                    if action_idx == 0:  # Buy
+                        # Первый элемент для объема закупа
+                        volume = int(prediction[0, 0].item() % 1000) + 1
+                        user.buy(volume, target_coin)
+                    elif action_idx == 1:  # Sell
+                        volume = int(prediction[0, 0].item() % 1000) + 1
+                        user.sell(volume, target_coin)
+                else:
+                    u_events = ['buy', 'sell']
+                    target = random.choice(world_manager[1])
+                    decision = random.choice(u_events)
+
+                    if decision == 'buy':
+                        user.buy(random.randint(1, 1000), target)
+                    elif decision == 'sell':
+                        user.sell(random.randint(1, 1000), target)
+
+
+
+
+
+            user.apply_request(percent_to_stock)
+
+            # запись богатства в историю
+            total_wealth = user.wc_count
+            for c in world_manager[1]:
+                total_wealth += user.wallet.get(c.name, 0) * c.currency
+            history['wealth'][user.nickname].append(total_wealth)
+
+        # запись курса монет в историю
+        for coin in world_manager[1]:
+            history['prices'][coin.name].append(coin.currency)
+
+    return history
+
+#--------------------------------------------------------
+# Построение графиков
+def graphics(history):
+    plt.style.use('ggplot')
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10))
+
+    # График цен
+    for coin_name, prices in history['prices'].items():
+        if coin_name != 'world_coin':
+            ax1.plot(prices, label=coin_name)
+    ax1.set_title('Динамика курсов монет')
+    ax1.set_ylabel('Цена в WC')
+    ax1.legend()
+
+    # График капитала
+    for user_nick, wealth_history in history['wealth'].items():
+        ax2.plot(wealth_history, label=user_nick)
+    ax2.set_title('Капитализация пользователей (Net Worth)')
+    ax2.set_ylabel('Общая стоимость активов')
+    ax2.set_xlabel('Шаги симуляции')
+    ax2.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+#--------------------------------------------------------
+
+if __name__ == '__main__':
+    h = main()
+    graphics(h)
---
+++ Stock.py
@@ -0,0 +1,126 @@
+import random
+import asyncio
+
+
+
+class Coin:
+    def __init__(self, name, invested_wc, coins_count):
+        # сид параметры монеты
+        self.name = name
+        self.invested = invested_wc
+        self.coins_count = coins_count
+
+        # основные параметры монеты
+        self.holders = {}
+        self.on_market = 0
+        self.currency = invested_wc / coins_count
+        self.capitalization = self.currency * self.on_market
+
+    @property
+    def draw_statistics(self):
+        return f'Current currency: {self.currency}, current holders count: {len(self.holders)}, capitalization: {self.capitalization}'
+
+
+
+# world_coin = Coin('world_coin', 10 ** 9, 10 ** 9)
+# print(world_coin.draw_statistics)
+
+class User:
+    def __init__(self, nickname='', wc_count=1000, wc_coin=None, coin_list = [],debug = False):
+        self.nickname = nickname
+        self.wc_count = wc_count
+        self.wc = wc_coin
+        self.coin_list = coin_list
+        self.debug = debug
+
+        self.wallet = {}
+        self.requests = []
+
+    def initialize_nick(self):
+        if self.nickname == '':
+            self.nickname = f'@{random.randint(0, 10000)}'
+        else:
+            self.nickname = f'@{self.nickname}'
+
+        print(f'Nickname has been initialized as {self.nickname}')
+
+    def register_in_wc(self):
+        self.wc.holders[self.nickname] = self.wc_count
+        self.wallet[self.wc.name] = self.wc_count
+        print(f'User: {self.nickname} registered in system')
+
+    @property
+    def draw_statistics(self):
+        return f'Nick: {self.nickname}, wallet: {self.wallet}, requests: {self.requests}'
+
+    # запрос покупки монеты
+    def buy(self, buy_count, coin: Coin):
+        # проверяем условия покупки
+        if self.wc_count < coin.currency * buy_count:
+            return f'{self.nickname} has not enough money to buy'
+        elif buy_count > coin.coins_count:
+            return f'{coin.name} below buy request'
+        else:
+            # создаем запрос покупки
+            self.wc_count -= coin.currency * buy_count
+            self.wallet[self.wc.name] = self.wc_count
+            self.requests.append(('buy', {coin.name: buy_count}))
+
+    # запрос продажи монеты
+    def sell(self, sell_count, coin: Coin):
+        self.requests.append(('sell', {coin.name: sell_count}))
+
+    # обработка запросов
+    def apply_request(self, precent_to_bank):
+        sum_to_bank = 0
+        for req_type, data in self.requests:
+            key = list(data.keys())[0]
+            amount = data[key]
+            coin = next(c for c in self.coin_list if c.name == key)
+
+            if req_type == 'buy':
+                fee_coins = (precent_to_bank / 100) * amount
+                sum_to_bank += fee_coins * coin.currency
+                coin.on_market += amount
+                coin.currency *= (1 + amount / coin.coins_count)
+                self.wallet[key] = self.wallet.get(key, 0) + (amount - fee_coins)
+
+            elif req_type == 'sell':
+                user_has = self.wallet.get(key, 0)
+                actual_sell = min(amount, user_has)
+                if actual_sell <= 0: continue
+
+                fee_wc = ((precent_to_bank - 0.5) / 100) * actual_sell * coin.currency
+                sum_to_bank += fee_wc
+                coin.on_market -= actual_sell
+
+                payout = (actual_sell * coin.currency) - fee_wc
+                self.wc_count += payout
+                self.wallet[self.wc.name] = self.wc_count
+                coin.currency *= (1 - actual_sell / coin.coins_count)
+                self.wallet[key] -= actual_sell
+
+            coin.capitalization = coin.currency * coin.on_market
+        self.requests.clear()
+        if self.debug == True:
+            print(self.draw_statistics)
+        return sum_to_bank
+
+# demo_coin = Coin('demo', 14, 100)
+
+# world_bank = [0,[world_coin, demo_coin]]
+# precent_to_bank = 15.0
+# commission = 2.0
+
+# demo_user = User('', 1000, world_coin,world_bank[1])
+# demo_user.initialize_nick()
+# demo_user.register_in_wc()
+
+# print(demo_user.draw_statistics)
+# demo_user.buy(10, demo_coin)
+
+# demo_user.sell(8, demo_coin)
+# print(demo_user.draw_statistics)
+
+# world_bank[0] += demo_user.apply_request(commission)
+# print(world_bank[0])
