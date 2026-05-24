25a22ea6d203d073efa8b4c410fdf6d8c5012697
FistHead <162723928+FistHead@users.noreply.github.com>
2026-05-24 15:59:50 +0300

    Added stocks, user and demo code of Social Network, StoryTeller model

--- Core.py
+++ Core.py
@@ -1,3 +1,149 @@
 import matplotlib.pyplot as plt
 import json
 import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+from torch.utils.data import Dataset,DataLoader
+
+from tokenizers import ByteLevelBPETokenizer
+from tokenizers.trainers import BpeTrainer
+from tokenizers import Tokenizer, models, pre_tokenizers, decoders, trainers, processors
+from tokenizers.models import BPE
+from tokenizers.pre_tokenizers import Whitespace
+import random
+from torchinfo import summary
+
+#============================================================================================
+
+class Stock:
+    def __init__(self, name, price, volume):
+        self.name = name
+        self.price = price
+
+        self.volume = volume
+        self.market_volume = 0
+
+        self.price_history = [price]
+        self.candles = []
+
+#============================================================================================
+
+class Request:
+    def __init__(self, user, type, stock, vol):
+        if type == 'buy':
+            stock.market_volume += vol
+            user.wallet['WC'] -= stock.price * vol
+            user.wallet[stock.name] = user.wallet.get(stock.name, 0) + vol
+            stock.price *= (1 + (vol / stock.volume))
+
+            stock.price_history.append(stock.price)
+        elif type == 'sell':
+            stock.market_volume -= vol
+            user.wallet['WC'] += stock.price * vol
+            user.wallet[stock.name] = user.wallet.get(stock.name, 0) - vol
+            stock.price *= (1 - (vol / stock.volume))
+
+            stock.price_history.append(stock.price)
+        else:
+            pass
+
+
+#============================================================================================
+class User:
+    def __init__(self, Name, Start_cap, Stocks, Model, idx, Network):
+        self.idx = idx
+        self.name = Name
+        self.network = Network
+
+        self.stocks = Stocks
+
+        self.wallet = {'WC': Start_cap}
+        self.requests = []
+
+        if Model != None:
+            self.model = Model
+        else:
+            raise Exception('Невозможно зарегистрировать пользователя без модели') 
+
+    def send_message(self, text: str):
+        self.network.global_chat[self.name] = text
+
+    def send_request(self, type, stock, vol):
+        cost = stock.price * vol
+        if type == 'buy':
+            if cost <= self.wallet['WC']:
+                if vol > stock.volume:
+                    return False, 0
+                if vol < 0:
+                    return False, 0
+                Request(self, type, stock, vol)
+                return True, vol
+            return False, 0
+
+        elif type == 'sell' and stock.name in self.wallet:
+            if vol > stock.volume:
+                return False, 0
+            if vol < 0:
+                return False, 0
+            if self.wallet.get(stock.name, 0) < vol:
+                return False, 0
+
+            Request(self, type, stock, vol)
+            return True, vol
+
+        return False, 0
+    
+    def get_total_wealth(self, all_stocks_prices):
+        total_stock_value = sum(self.wallet.get(name, 0) * price for name, price in all_stocks_prices.items())
+        return self.wallet['WC'] + total_stock_value
+        
+#============================================================================================
+
+class SocialNetwork:
+    def __init__(self, Name, Users):
+        self.name = Name
+        self.users = Users
+        self.global_chat = {}
+
+    def __len__(self):
+        return len(self.users)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def remove_user(self, user):
+        self.users.remove(user)
+
+#============================================================================================
+
+class StoryTeller(nn.Module):
+    def __init__(self, window_size: int, vocab_size: int, emb_size: int, rules_count: int):
+        super(StoryTeller, self).__init__()
+
+        self.time_window = nn.Sequential(
+            nn.Linear(window_size, emb_size),
+            nn.Linear(emb_size, rules_count)
+        )
+
+        self.chat_inflation = nn.Linear(emb_size, rules_count)
+
+        self.embedding = nn.Embedding(vocab_size, emb_size)
+        self.summary_layer = nn.Linear(rules_count * 2, rules_count)
+
+    def forward(self, text, history):
+        emb = self.embedding(text)
+        history = self.time_window(history)
+        emb_mean = torch.mean(emb, dim=1)
+        chat_inf = self.chat_inflation(emb_mean)
+        
+        all_feautures = torch.cat((history, chat_inf), dim=1)
+        res = self.summary_layer(all_feautures)
+
+        return res
+
+#============================================================================================
+
